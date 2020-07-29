5dd25d51e1dac3c8c7ab55891aeec6876b2023a9
Juan Vidal <juan.vidal@daliaresearch.com>
2020-07-29 10:30:58 +0200

    adding Rust async event consumer

---
+++ consumer/src/main.rs
@@ -0,0 +1,79 @@
+use futures::*;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance, StreamConsumer};
+use rdkafka::error::KafkaResult;
+use rdkafka::{ClientContext, Message, TopicPartitionList};
+
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        println!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        println!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {}
+}
+
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
+    let context = CustomContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&topics.to_vec())
+        .expect("Can't subscribe to specified topics");
+
+    let mut message_stream = consumer.start();
+
+    while let Some(message) = message_stream.next().await {
+        match message {
+            Err(e) => println!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        println!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    // consume
+    let task1 = async {
+        let brokers = "localhost:9092";
+        let topics = vec!["verwandlung"];
+        let group_id = "001";
+        consume_and_print(brokers, group_id, &topics).await;
+    };
+    rt.spawn(task1);
+
+    tokio::signal::ctrl_c().await?;
+    println!("ctrl-c received!");
+    Ok(())
+}
